08aa9c38346bbcc8d2f605837cb0412bb59f9efd
gajendra-github <gajendra.github@gmail.com>
2023-03-06 11:38:17 -0500

    added sessions router

--- app.js
+++ app.js
@@ -4,16 +4,44 @@ const debug = require('debug')('app');
 const morgan = require('morgan');   //logs web traffic
 require('dotenv').config();
 const path = require('path');
+const sessions = require('./src/data/sessions.json');
 
+
+
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
+const sessionsRouter = express.Router();
+
+sessionsRouter.route('/').get((req, res) => {
+    //res.send('hello from sessions');
+    res.render('sessions', {
+        sessions,
+        // sessions: [
+        //     { title: 'Session 1', description: 'Sessions 1 description' },
+        //     { title: 'Session 2', description: 'Sessions 2 description' },
+        //     { title: 'Session 3', description: 'Sessions 3 description' },
+        //     { title: 'Session 4', description: 'Sessions 4 description' }
+        // ]
+    })
+});
+sessionsRouter.route('/:id').get((req, res) => {
+    const id = req.params.id;
+    //res.send(`hello from single session ${id}`);
+    res.render('session', {
+        session: sessions[id],
+    });
+});
+
 
 app.use(morgan('tiny'));
 //app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use('/sessions', sessionsRouter);    //sessions --> sessionRouter (contains all code to deal with sessions route)
+app.get('/sessions/sessionID');
+
 
 app.get('/', (req, res) => {
     //res.send('Hello, World!');
