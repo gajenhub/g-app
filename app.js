205a20b49eb2b61eed028f31c754669d593254c6
gajendra-github <gajendra.github@gmail.com>
2023-03-06 09:18:11 -0500

    added template ejs

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-
-//allow us to set some color on our messages..so we can group them together
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');   //logs web traffic
@@ -11,34 +9,53 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(morgan('tiny'));
+//app.use(express.static(path.join(__dirname, '/public/')));
 
-//app.use(morgan('combined'));      //gives lot of info about page
-app.use(morgan('tiny'));            //changed settings to tiny for less information
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '/public/')));
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    //res.send('Hello, World!');
+    res.render('index', {
+        title: ''
+    });
 })
 
 app.listen(port, () => {
-    //console.log(`listening to port ${chalk.green(port)}`);
-
-
     debug(`Listening to port ${chalk.green(port)}`);
-    //only runs in debug mode, in production doesnot print in console.
-    //$DEBUG=* node app.js
-    //logs web traffic
-
-
 })
 
 
-/*
-Note:
 
-e.g. $npm start; $npm test; $npm run debug
-nodemon:        watch for changes and restart the app
-environment variables priorities:
-            -   package.json
-            -   .env file
+
+
+
+/*
+chalk:
+//allow us to set some color on our messages..so we can group them together
+
+morgan:
+//app.use(morgan('combined'));      //gives lot of info about page
+//changed settings to tiny for less information
+
+app.use:
+//allows to set variables inside the context of our application
+//setting a variable called 'views' and telling where the views are src/views
+
+//app.set:
+//setting view engine name
+//expess app looking in src/views for templates associated with EJS. 
+
+
+//console.log(`listening to port ${chalk.green(port)}`);
+//only runs in debug mode, in production doesnot print in console.
+//$DEBUG=* node app.js
+//logs web traffic
+
+using following code will override view engine ejs and take index page from public folder.
+//app.use(express.static(path.join(__dirname, '/public/')));
+
+
+
 */
---
+++ src/doc.js
@@ -0,0 +1,8 @@
+/*
+
+
+
+Templating Engines:
+
+
+*/
