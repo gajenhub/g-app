a54562ccaf9573781ee7536b7347339632ed2c9f
gajendra-github <gajendra.github@gmail.com>
2023-03-06 11:52:01 -0500

    added sessionsRouter.js

--- app.js
+++ app.js
@@ -4,34 +4,11 @@ const debug = require('debug')('app');
 const morgan = require('morgan');   //logs web traffic
 require('dotenv').config();
 const path = require('path');
-const sessions = require('./src/data/sessions.json');
-
-
-
 const port = process.env.PORT || 3000;
 
 const app = express();
-const sessionsRouter = express.Router();
-
-sessionsRouter.route('/').get((req, res) => {
-    //res.send('hello from sessions');
-    res.render('sessions', {
-        sessions,
-        // sessions: [
-        //     { title: 'Session 1', description: 'Sessions 1 description' },
-        //     { title: 'Session 2', description: 'Sessions 2 description' },
-        //     { title: 'Session 3', description: 'Sessions 3 description' },
-        //     { title: 'Session 4', description: 'Sessions 4 description' }
-        // ]
-    })
-});
-sessionsRouter.route('/:id').get((req, res) => {
-    const id = req.params.id;
-    //res.send(`hello from single session ${id}`);
-    res.render('session', {
-        session: sessions[id],
-    });
-});
+//const sessionsRouter = express.Router();
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 
 app.use(morgan('tiny'));
@@ -40,7 +17,7 @@ app.use(morgan('tiny'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use('/sessions', sessionsRouter);    //sessions --> sessionRouter (contains all code to deal with sessions route)
-app.get('/sessions/sessionID');
+
 
 
 app.get('/', (req, res) => {
---
+++ src/routers/sessionsRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const sessionsRouter = express.Router();
+//const sessions = require('./src/data/sessions.json');
+const sessions = require('../data/sessions.json');
+
+sessionsRouter.route('/').get((req, res) => {
+    //res.send('hello from sessions');
+    res.render('sessions', {
+        sessions,
+        // sessions: [
+        //     { title: 'Session 1', description: 'Sessions 1 description' },
+        //     { title: 'Session 2', description: 'Sessions 2 description' },
+        //     { title: 'Session 3', description: 'Sessions 3 description' },
+        //     { title: 'Session 4', description: 'Sessions 4 description' }
+        // ]
+    })
+});
+sessionsRouter.route('/:id').get((req, res) => {
+    const id = req.params.id;
+    //res.send(`hello from single session ${id}`);
+    res.render('session', {
+        session: sessions[id],
+    });
+});
+
+module.exports = sessionsRouter;
+
