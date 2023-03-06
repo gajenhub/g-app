bf5fc122a5073f15757d10f0bf091530516b89f1
gajendra-github <gajendra.github@gmail.com>
2023-03-06 08:07:13 -0500

    added debug:logs message in dev env, added morgan: logs web traffic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const chalk = require('chalk');
-//allow us to set some color on our messages..so we can group them together
 
+//allow us to set some color on our messages..so we can group them together
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');   //logs web traffic
 require('dotenv').config();
 const path = require('path');
 
@@ -9,13 +11,23 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//app.use(morgan('combined'));      //gives lot of info about page
+app.use(morgan('tiny'));            //changed settings to tiny for less information
+
 app.use(express.static(path.join(__dirname, '/public/')));
-
-
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
 
 app.listen(port, () => {
-    console.log(`listening to port ${chalk.green(port)}`);
+    //console.log(`listening to port ${chalk.green(port)}`);
+
+
+    debug(`listening to port ${chalk.green(port)}`);
+    //only runs in debug mode, in production doesnot print in console.
+    //$DEBUG=* node app.js
+    //logs wef traffic
+
+
 })
