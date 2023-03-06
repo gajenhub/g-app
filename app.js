c4cc29699c89b5b6e85e22d506a594475276853a
gajendra-github <gajendra.github@gmail.com>
2023-03-06 07:22:59 -0500

    added middleware to access public/index

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/public/')));
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
