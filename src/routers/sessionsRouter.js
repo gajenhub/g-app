6ab27822956178e4609349bddfc790b1b06aa970
gajendra-github <gajendra.github@gmail.com>
2023-03-08 09:58:11 -0500

    update sessionRouter to fetch data from MongoDb

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const debug = require('debug')('app:sessionRouter');
-//const sessions = require('./src/data/sessions.json');
-//const sessions = require('../data/sessions.json');
-const { MongoClient } = require('mongodb');
-
-
+const { MongoClient, ObjectId } = require('mongodb');
 
 const sessionsRouter = express.Router();
 
@@ -32,11 +28,28 @@ sessionsRouter.route('/').get((req, res) => {
 
 sessionsRouter.route('/:id').get((req, res) => {
     const id = req.params.id;
-    //res.send(`hello from single session ${id}`);
-    res.render('session', {
-        session: sessions[id],
-    });
 
+    const uri = process.env.LOCAL_ACCESS_MONGODB_URI || process.env.HEROKU_MONGODB_URI;
+
+    (async function mongo() {
+        let client;
+        try {
+
+            client = await MongoClient.connect(uri);
+            debug('Connected to the mongo DB');
+            const db = client.db("mydb");
+            const session = await db.collection('sessions').findOne(
+                { _id: new ObjectId(id) });
+
+            res.render('session', {
+                session,
+            });
+
+        } catch (error) {
+            console.log(error.stack);
+        }
+
+    }());
 });
 
 module.exports = sessionsRouter;
