f93e9c53a36cc1ddcf13f788c30a2dfb502b7c85
gajendra-github <gajendra.github@gmail.com>
2023-03-08 09:12:03 -0500

    update sessionRouter code to get data from MongoDb

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,27 +1,42 @@
 const express = require('express');
+const debug = require('debug')('app:sessionRouter');
 //const sessions = require('./src/data/sessions.json');
 const sessions = require('../data/sessions.json');
+const { MongoClient } = require('mongodb');
+
+
+
 const sessionsRouter = express.Router();
 
 sessionsRouter.route('/').get((req, res) => {
-    //res.send('hello from sessions');
-    res.render('sessions', {
-        sessions,
-        // sessions: [
-        //     { title: 'Session 1', description: 'Sessions 1 description' },
-        //     { title: 'Session 2', description: 'Sessions 2 description' },
-        //     { title: 'Session 3', description: 'Sessions 3 description' },
-        //     { title: 'Session 4', description: 'Sessions 4 description' }
-        // ]
-    })
+    const uri = process.env.LOCAL_ACCESS_MONGODB_URI || process.env.HEROKU_MONGODB_URI;
+
+    (async function mongo() {
+        let client;
+        try {
+
+            client = await MongoClient.connect(uri);
+            debug('Connected to the mongo DB');
+
+            const db = client.db("mydb");
+            const sessions = await db.collection('sessions').find().toArray();
+            res.render('sessions', { sessions })
+
+        } catch (error) {
+            console.log(error.stack);
+        }
+
+    }());
+
 });
+
 sessionsRouter.route('/:id').get((req, res) => {
     const id = req.params.id;
     //res.send(`hello from single session ${id}`);
     res.render('session', {
         session: sessions[id],
     });
+
 });
 
 module.exports = sessionsRouter;
-
